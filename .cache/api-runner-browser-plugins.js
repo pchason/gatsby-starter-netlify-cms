c903de50b270d5e91abcf842bba4ec374bd25b3f
Paul Chason <paulchason@gmail.com>
2021-01-13 07:13:08 -0500

    Removed .cache and public dir's and got them out of cache.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-twitter/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-theme-serenity/gatsby-browser.js'),
-      options: {"plugins":[],"title":"Paul Chason","subtitle":"a short subtitle for the meta title on the home page","description":"A minimal theme for your personal blog or webpage","siteUrl":"https://paulchason.com","defaultPreviewImage":"/og-default.png","author":{"name":"Paul Chason","bio":"Entrepreneuer","url":"https://paulchason.com","links":{"twitter":"https://twitter.com/paulchason","linkedin":"https://linkedin.com/in/paulchason","github":"https://github.com/pchason","instagram":"https://instagram.com/chasonp","mail":"mailto:paulchason@gmail.com"}},"links":[{"title":"Articles","href":"/"},{"title":"About me","href":"/info/about-me"}],"articles_per_page":25,"language":"en"},
-    }]
