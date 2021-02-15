50d140eed9ceccc8da22ddcf08b72e23630bce3d
Paul Chason <paulchason@gmail.com>
2021-02-15 07:14:51 -0500

    Updated Comments component to create a reference to inject utteranc.es script element so it loads consistently.

--- src/gatsby-theme-serenity/components/Comments.js
+++ src/gatsby-theme-serenity/components/Comments.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 
 export default class Comments extends Component {
-    
+
+    constructor(props){ 
+        super(props);
+        this.commentBox = React.createRef(); // Creates a reference to inject the <script> element
+    }
+
+    componentDidMount () {
+        let scriptEl = document.createElement("script");
+        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+        scriptEl.setAttribute("crossorigin","anonymous");
+        scriptEl.setAttribute("async", true);
+        scriptEl.setAttribute("repo", "pchason/paulchason.com-gatsby-netlify");
+        scriptEl.setAttribute("issue-term", "pathname");
+        scriptEl.setAttribute( "theme", "github-light");
+        this.commentBox.current.appendChild(scriptEl);
+    }
+
     render() {
     return (
            <div>
-            <script src="https://utteranc.es/client.js"
-                repo="pchason/paulchason.com-gatsby-netlify"
-                issue-term="pathname"
-                theme="github-light"
-                crossorigin="anonymous"
-                async>
-            </script>
+            <div ref={this.commentBox} className="comment-box"/>
+            {/* Above element is where the comments are injected */}
            </div>
            );
     }
